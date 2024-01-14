0c16209a7e67fdd34914194e602068205dd34ae3
Lucas-PC <lgbs@cesar.school>
2024-01-14 01:28:07 -0300

    adding a way to get the wpm and time taken to conclude the tests

--- src/main.py
+++ src/main.py
@@ -1,10 +1,15 @@
 '''Main file of the project. Run this file to start the program.'''
+import os
 from typing_bot import TypingBot
 
 typeBot = TypingBot()
 
 # https://typethealphabet.app
-typeBot.write_alfabet()
+tempo1 = typeBot.write_alfabet()
 
 # https://monkeytype.com
-typeBot.monkey_type()
+tempo2 = typeBot.monkey_type()
+
+os.system('cls' if os.name == 'nt' else 'clear')
+print(tempo1)
+print(tempo2)
--- src/typing_bot.py
+++ src/typing_bot.py
@@ -5,6 +5,7 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException, NoSuchElementException, TimeoutException
 
 class TypingBot:
     '''Classe que representa o bot que será executado nos sites de digitação'''
@@ -30,8 +31,13 @@ class TypingBot:
 
         ActionChains(self.driver).send_keys(alfabet).perform()
 
+        # Para pegar o tempo que demorou para digitar
+        tempo = self.driver.find_element(By.CLASS_NAME, 'tabular-nums').text
+        tempo = f'\033[92mTempo em Type The Alphabet: {tempo.split(' ')[1]}\033[0m'
+
         sleep(5)
         self.driver.close()
+        return tempo
 
     def monkey_type(self):
         '''Faz o teste de velocidade de escrita no site MonkeyType'''
@@ -44,19 +50,29 @@ class TypingBot:
         self.driver.find_element(By.TAG_NAME, 'button').click()
         sleep(0.5)
 
-        while self.driver.find_elements(By.CLASS_NAME, 'time'):
-            # Aguardar até que a palavra esteja presente
-            palavra = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.CLASS_NAME, 'word.active'))
-            )
+        try:
+            while self.driver.find_elements(By.CLASS_NAME, 'time'):
+                # Aguardar até que a palavra esteja presente
+                palavra = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((By.CLASS_NAME, 'word.active'))
+                )
 
-            letras = palavra.find_elements(By.TAG_NAME, 'letter')
+                letras = palavra.find_elements(By.TAG_NAME, 'letter')
 
-            # Enviar as letras e pressionar a barra de espaço
-            ActionChains(self.driver)\
-                .send_keys(''.join([letra.text for letra in letras]) + ' ')\
-                .perform()
-
+                # Enviar as letras e pressionar a barra de espaço
+                ActionChains(self.driver)\
+                    .send_keys(''.join([letra.text for letra in letras]) + ' ')\
+                    .perform()
+        except (StaleElementReferenceException,
+                NoSuchElementException,
+                TimeoutException):
+            pass
+        
         sleep(5)
+        # Para pegar a quantidade de palavras por minuto
+        stats = self.driver.find_element(By.CLASS_NAME, 'stats')
+        wpm = stats.find_element(By.CLASS_NAME, 'bottom').text
+        wpm = f'\033[92mPalavras por minuto em MonkeyType: {wpm}\033[0m'
 
         self.driver.close()
+        return wpm
